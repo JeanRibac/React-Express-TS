ac57b8a3546fde24066a28db6af24bd749307fc5
JeanRibac <53911977+JeanRibac@users.noreply.github.com>
2020-08-11 15:50:40 +0300

    Update Constants.ts

--- server/src/config/Constants.ts
+++ server/src/config/Constants.ts
@@ -1,5 +1,5 @@
-export const secret = "SecretSmecher%SiBengos123456";
-export const MONGODB_URI = "mongodb+srv://jeanribac95:1234@cluster0.xzehe.mongodb.net/<dbname>?retryWrites=true&w=majority";
-export const CLOUD_NAME = "digitalpoint";
-export const API_KEY = "597226554535138";
-export const API_SECRET = "UL8dl4hQY2zBnNM-5Ksqwah-5MA"
+export const secret = "";
+export const MONGODB_URI = "";
+export const CLOUD_NAME = "";
+export const API_KEY = "";
+export const API_SECRET = ""
